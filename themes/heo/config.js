f124f640642b09d9bb3b8e0df3224f316a982fad
craigchan <chan.craig@icloud.com>
2025-04-01 14:09:34 +0800

    Update config.js
    theme change by craigchan

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+    { title: '欢迎来到我的博客', url: 'https://wzardai.life' },
     { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
@@ -18,12 +18,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
+  HEO_HERO_TITLE_1: '分享AI工具测评',
+  HEO_HERO_TITLE_2: '与自我探索行为实验',
   HEO_HERO_TITLE_3: 'WzardAI.life',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_4: '最新思维快照',
+  HEO_HERO_TITLE_5: '只有完成自我，才能获得健康人际关系',
+  HEO_HERO_TITLE_LINK: 'https://wzardai.life/article/thinking01',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
